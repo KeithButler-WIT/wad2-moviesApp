23c156229d8ee82cbf16c2e7f0dec10906ce2854
KeithButler-WIT <20089137@mail.wit.ie>
2021-11-01 15:09:05 +0000

    Show movie review excerpts; Refactor details page

--- src/components/movieDetails/index.js
+++ src/components/movieDetails/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Chip from "@material-ui/core/Chip";
 import Paper from "@material-ui/core/Paper";
 import AccessTimeIcon from "@material-ui/icons/AccessTime";
@@ -8,6 +8,8 @@ import NavigationIcon from "@material-ui/icons/Navigation";
 import Fab from "@material-ui/core/Fab";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import MovieReviews from "../movieReviews";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -28,9 +30,9 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const MovieDetails = ( props) => {
+const MovieDetails = ({ movie }) => {
   const classes = useStyles();
-  const movie = props.movie
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
   return (
     <>
@@ -75,14 +77,18 @@ const MovieDetails = ( props) => {
         ))}
       </Paper>
 
-      <Fab
+     <Fab
         color="secondary"
         variant="extended"
+        onClick={() =>setDrawerOpen(true)}
         className={classes.fab}
       >
         <NavigationIcon />
         Reviews
       </Fab>
+      <Drawer anchor="top" open={drawerOpen} onClose={() => setDrawerOpen(false)}>
+        <MovieReviews movie={movie} />
+      </Drawer>
       </>
   );
 };
