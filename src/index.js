984d66eef6fea3a128a0a1aee06b2be030a92bda
Keith Butler <20089137@mail.wit.ie>
2021-11-01 12:19:56 +0000

    Movies details page rendering

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import HomePage from "./pages/homePage";
+import MovieDetailsPage from './pages/movieDetailsPage';
+
+const images = [
+  "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
+  "/v1QQKq8M0fWxMgSdGOX1aCv8qMB.jpg",
+  "/2iGN0aKHJYD0xQydlfuCUAcgNbO.jpg",
+  "/rjBwhsOzHKUw2NIOrE7aMqjfe6s.jpg",
+]
 
 const sample = {
   adult: false,
@@ -87,7 +95,7 @@ const movies = [sample, sample, sample, sample, sample, sample, sample];
 
 const App = () => {
   return (
-      <HomePage movies={movies} />
+    <MovieDetailsPage movie={sample} images={images} />
   );
 };
 
