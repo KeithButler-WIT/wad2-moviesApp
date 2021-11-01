fba2b094572afe687fb955ca3f62ad16ae2dc41f
KeithButler-WIT <20089137@mail.wit.ie>
2021-11-01 15:29:48 +0000

    Fix backward/forward arows

--- src/components/headerMovieList/index.js
+++ src/components/headerMovieList/index.js
@@ -5,6 +5,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
+import { withRouter } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -15,27 +16,20 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const Header = (props ) => {
+const Header = ({ title, history }) => {
   const classes = useStyles();
-  const title = props.title
   return (
     <Paper component="div" className={classes.root}>
-      <IconButton
-        aria-label="go back"
-      >
+      <IconButton aria-label="go back" onClick={() => history.goBack()}>
         <ArrowBackIcon color="primary" fontSize="large" />
       </IconButton>
-
       <Typography variant="h4" component="h3">
         {title}
       </Typography>
-      <IconButton
-        aria-label="go forward"
-      >
+      <IconButton aria-label="go forward" onClick={() => history.goForward()}>
         <ArrowForwardIcon color="primary" fontSize="large" />
-      </IconButton>
-    </Paper>
+      </IconButton>    </Paper>
   );
 };
 
-export default Header;
+export default withRouter(Header);
